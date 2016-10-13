302308380fc90b7985dc3ba5db5287a2a4f6ae52
Boris_il_forte <davide.tateo90@gmail.com>
2016-10-13 22:07:51 +0200

    Added IR node to rpoximity module

--- targets/proximity/main.cpp
+++ targets/proximity/main.cpp
@@ -17,6 +17,7 @@ Module module;
 
 // NODES
 core::led::Subscriber led_subscriber("led_subscriber", core::os::Thread::PriorityEnum::LOWEST);
+core::ir_publisher::IRNode ir_publisher("ir_publisher", core::os::Thread::PriorityEnum::NORMAL);
 
 // MAIN
 extern "C" {
@@ -26,11 +27,18 @@ extern "C" {
       module.initialize();
 
       module.add(led_subscriber);
+      module.add(ir_publisher);
 
       // Led subscriber node
       core::led::SubscriberConfiguration led_subscriber_configuration;
       led_subscriber_configuration.topic = "led";
       led_subscriber.setConfiguration(led_subscriber_configuration);
+
+      //IR node
+      core::ir_publisher::IRNodeConfiguration ir_conf;
+      ir_conf.topic = "proximity";
+      ir_publisher.setConfiguration(ir_conf);
+
 
       // Setup and run
       module.setup();
